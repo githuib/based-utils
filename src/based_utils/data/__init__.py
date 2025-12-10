ff2bc6be288b18542b262ff3a735eeac45b2dd8f
Huib Piguillet <huib@proton.me>
2025-12-10 17:41:00 +0100

    cleanup

--- src/based_utils/data/__init__.py
+++ src/based_utils/data/__init__.py
@@ -1,3 +1,6 @@
 from .conversion import get_class_vars, try_convert
 
+PRE_a = ord("a") - 1
+PRE_A = ord("A") - 1
+
 __all__ = ["get_class_vars", "try_convert"]
--- src/based_utils/data/strings.py
+++
@@ -1,6 +0,0 @@
-
-
-
-
-PRE_a = ord("a") - 1
-PRE_A = ord("A") - 1
