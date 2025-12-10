747625a2dcda14284216e0793ec6eb39b01d636c
Huib Piguillet <huib@proton.me>
2025-12-10 02:56:32 +0100

    Forget to add some stuff to __all__

--- src/based_utils/cli/__init__.py
+++ src/based_utils/cli/__init__.py
@@ -1,5 +1,10 @@
 from .animation import Lines, animate, animated
-from .args import check_integer, check_integer_in_range
+from .args import (
+    check_integer,
+    check_integer_in_range,
+    parse_key_value_pair,
+    try_parse_key_value_pair,
+)
 from .clox import human_readable_duration, timed, timed_awaitable
 from .exec import FatalError, killed_by_errors
 from .formats import Colored
@@ -20,6 +25,8 @@ __all__ = [
     "format_table",
     "human_readable_duration",
     "killed_by_errors",
+    "parse_key_value_pair",
     "timed",
     "timed_awaitable",
+    "try_parse_key_value_pair",
 ]
